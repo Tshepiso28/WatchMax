05c5a3b4f40b28f60feac5f3366250346c821e6a
Tshepiso28 <tshepisostudent@gmail.com>
2025-03-17 09:08:29 +0200

    removed the routing module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router'; // Import provideRouter
-import { routes } from './app.routes'; // Import your routes
+import { provideRouter } from '@angular/router'; 
+import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(), // Enables HTTP
-    provideRouter(routes), // Enables routing
+    provideHttpClient(), 
+    provideRouter(routes), 
   ],
 };
